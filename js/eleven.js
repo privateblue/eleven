d869342c57defe17799469703199857d62bab26f
Mikolaj Szabo <savingprivateblue@gmail.com>
2018-01-03 16:45:07 +0000

    Minimalistic design with no animations

--- js/eleven.js
+++ js/eleven.js
@@ -1,30 +1,29 @@
 const players = 1;
 const machines = [];
 
-const colorMap = [[0,0,255], [255,0,0], [0,255,0]];
+const colorMap = [[81,134,198], [229,87,16], [190,255,117]];
 const directionKeyMap = [39, 37, 40, 38];
 const directionSymbolMap = ['→', '←', '↓', '↑'];
 
 const width = window.innerWidth;
 const height = window.innerHeight;
-const size = Math.min(width, height);                     // shorter side of screen
-const cx = Math.round(width / 2);                         // horizontal center of screen
-const cy = Math.round(height / 2);                        // vertical center of screen
-const r = Math.round(size / 25);                          // initial radius of value circle
-const str = Math.round(r / 3);                            // base stroke width factor
-const s = Math.round(2.5 * r);                            // spacing factor btw value circles
-const scoreY = Math.round(1.5 * r);                       // score bar vertical position
-const scoreW = Math.round(width / players / 2 - 2 * str); // spacing of score bar elements
-const fontSize = Math.round(r / 2);                       // font size
+const size = Math.min(width, height);           // shorter side of screen
+const cx = Math.round(width / 2);               // horizontal center of screen
+const cy = Math.round(height / 2);              // vertical center of screen
+const r = Math.round(size / 25);                // initial radius of value circle
+const str = Math.round(r / 3);                  // base stroke width factor
+const s = Math.round(2.5 * r);                  // spacing factor btw value circles
+const scoreY = Math.round(1.5 * r);             // score bar vertical position
+const fontSize = Math.round(r / 2);             // font size
+const delay = 200;                              // delay between moves
 
 var circles = [];
 var vals = [];
+var scrs = [];
 
-var scoreBar;
 var layer;
 
 var player = 0;
-var n = 0;
 
 initBoard(Game.graphOriginal);
 updateBoard(Game.graphOriginal);
@@ -48,7 +47,7 @@ function move(g) {
       var ep = es => new Promise((res, rej) => res(Game.indexOf(best.put)));
       var dp = (graph, ds) => new Promise(function(res, rej) {
         updateBoard(graph);
-        setTimeout(() => res(Game.directionOf(best.dir)), 500);
+        setTimeout(() => res(Game.directionOf(best.dir)), delay);
       });
     // human player moves
     } else {
@@ -97,23 +96,21 @@ function handleResult(entry, graph, scores) {
   updateBoard(graph);
   updateScores(scores, entry);
   player = (player + 1) % players;
-  n = n + 1;
-  return new Promise((resolve, reject) => setTimeout(() => resolve(), 500));
+  return new Promise((resolve, reject) => setTimeout(() => resolve(), delay));
 }
 
 function updateBoard(g) {
   var graph = Game.graphToJs(g);
   for (i = 0; i < graph.values.length; i++) {
     if ("c" in graph.values[i]) {
-      var c = colorMap[graph.values[i].c].slice();
-      c.push(0.5);
       circles[i].radius((1 + graph.values[i].v / 10) * r);
-      circles[i].fill(color(c));
+      circles[i].fill(color(colorMap[graph.values[i].c]));
     } else {
       circles[i].radius(r);
       circles[i].fill(color([255,255,255,0]));
     }
     vals[i].setAttr('text', graph.values[i].v);
+    vals[i].setOffset({x: vals[i].getWidth() / 2, y: vals[i].getHeight() / 2});
     vals[i].visible(graph.values[i].v != 0);
   }
   layer.draw();
@@ -122,49 +119,20 @@ function updateBoard(g) {
 function updateScores(ss, he) {
   var scores = Game.scoresToJs(ss);
   var entry = Game.historyEntryToJs(he);
-
-  var cur = scoreBar.getChildren(c => c.getName() == 'gombocka-' + n)[0];
-  cur.strokeEnabled(false);
-  var nxt = scoreBar.getChildren(c => c.getName() == 'gombocka-' + (n + 1))[0];
-
-  var scr = scoreBar.getChildren(c => c.getName() == 'score-' + n)[0];
-  scr.setAttr('text', '' + scores[player]);
-  scr.setOffset({ x: scr.getWidth() / 2, y: 0 });
-
-  var lastm = scoreBar.getChildren(c => c.getName() == 'lastm-' + n)[0];
-  if ('dir' in entry) lastm.setAttr('text', directionSymbolMap[entry.dir]);
-  lastm.setOffset({ x: lastm.getWidth() / 2, y: 0 });
-
-  scoreBar
-    .getChildren(c => c.getName() == 'gombocka-' + (n + 1))
-    .forEach(c => c.opacity(1));
-
-  scoreBar.getChildren(c =>
-    parseInt(c.getName().match(/\d+/i)[0]) < n - players
-  ).forEach(c => c.hide());
-
-  appendScoreBar(players + n + 1);
-
-  var shift = new Konva.Tween({
-    node: scoreBar,
-    easing: Konva.Easings.EaseInOut,
-    duration: 0.5,
-    x: (n + 1) * -scoreW,
-    onFinish: function() {
-      nxt.stroke('black');
-      nxt.strokeWidth(str / 2);
-    }
-  });
-  shift.play();
+  var scr = scrs[player];
+  scr.scr.setAttr('text', '' + scores[player]);
+  scr.scr.setOffset({ x: scr.scr.getWidth() / 2, y: 0 });
+  if ('dir' in entry) scr.lastm.setAttr('text', directionSymbolMap[entry.dir]);
+  scr.lastm.setOffset({ x: scr.lastm.getWidth() / 2, y: 0 });
+  scr.gombocka.scale({x: 1, y: 1});
+  scrs[(player + 1) % players].gombocka.scale({x: 1.5, y: 1.5});
+  layer.draw();
 }
 
 function gameOver(msg) {
-  scoreBar.getChildren(c =>
-    parseInt(c.getName().match(/\d+/i)[0]) >= n
-  ).forEach(c => c.hide());
-  scoreBar.getChildren(c => c.getName() == 'line-' + (n - 1))[0].hide();
+  scrs[player].gombocka.scale({x: 1, y: 1});
   var message = new Konva.Text({
-    x: cx + (n - 1) * scoreW + 2 * str,
+    x: r + players * 2 * r,
     y: scoreY,
     text: msg,
     fontFamily: 'Dosis',
@@ -173,7 +141,7 @@ function gameOver(msg) {
     fill: 'black'
   });
   message.setOffset({x: 0, y: message.getHeight() / 2});
-  scoreBar.add(message);
+  layer.add(message);
   layer.draw();
 }
 
@@ -183,15 +151,50 @@ function initBoard(g) {
   layer = new Konva.Layer();
 
   // scores
-  scoreBar = new Konva.Group({});
-  for (i = 0; i < players + 1; i++) {
-    appendScoreBar(i)
+  var line = new Konva.Line({
+    points: [0, scoreY, width, scoreY],
+    stroke: 'black',
+    strokeWidth: 0.1
+  });
+  layer.add(line);
+  for (i = 0; i < players; i++) {
+    var xi = r + i * 2 * r;
+    var gombocka = new Konva.Circle({
+      name: 'gombocka-' + i,
+      x: xi,
+      y: scoreY,
+      radius: str,
+      fill: color(colorMap[i])
+    });
+    layer.add(gombocka);
+    var score = new Konva.Text({
+      name: 'score-' + i,
+      x:  xi,
+      y: scoreY - fontSize - 2 * str,
+      text: '',
+      fontFamily: 'Dosis',
+      fontStyle: 'bold',
+      fontSize: fontSize,
+      fill: 'black'
+    });
+    score.setOffset({x: score.getWidth() / 2, y: 0});
+    layer.add(score);
+    var lastm = new Konva.Text({
+      name: 'lastm-' + i,
+      x: xi,
+      y: scoreY + 2 * str,
+      text: '',
+      fontFamily: 'Dosis',
+      fontStyle: 'bold',
+      fontSize: fontSize,
+      fill: 'black'
+    });
+    lastm.setOffset({x: lastm.getWidth() / 2, y: 0});
+    scrs.push({gombocka: gombocka, scr: score, lastm: lastm});
+    layer.add(lastm);
   }
-  var cur = scoreBar.getChildren(c => c.getName() == 'gombocka-0')[0]
-  cur.opacity(1);
-  cur.stroke('black');
-  cur.strokeWidth(str / 2);
-  layer.add(scoreBar);
+  var cur = layer.getChildren(c => c.getName() == 'gombocka-0')[0];
+  cur.scale({x: 1.5, y: 1.5});
 
   // circles
   for (y = cy - (3 * s); y <= cy + (3 * s); y = y + s + s) {
@@ -212,13 +215,14 @@ function initBoard(g) {
       circles.push(c);
       layer.add(c);
       var t = new Konva.Text({
-        x: x + str,
-        y: y - r / 2 - str,
+        x: x,
+        y: y,
         text: '0',
         fontFamily: 'Dosis',
         fontStyle: 'bold',
-        fontSize: fontSize
+        fontSize: 1.5 * fontSize
       });
+      t.setOffset({x: t.getWidth() / 2, y: t.getHeight() / 2});
       t.visible(false);
       vals.push(t);
       layer.add(t);
@@ -237,15 +241,14 @@ function initBoard(g) {
       var y2 = to.attrs.y;
       var a = new Konva.Arrow({
         points: arrowPoints(x1, y1, x2, y2, r, str),
-        tension: 1,
-        pointerLength: str,
-        pointerWidth: str / 2,
+        pointerLength: 0,
+        pointerWidth: 0,
         fill: 'black',
         stroke: 'black',
-        strokeWidth: 1,
-        opacity: 0.1
+        strokeWidth: 0.5
       });
       layer.add(a);
+      a.setZIndex(0);
     }
   }
 
@@ -271,51 +274,6 @@ function arrowPoints(x1, y1, x2, y2, r, str) {
     // right
     return [x1 + str, y1, x2 - str, y2];
   }
-}
-
-function appendScoreBar(i) {
-  var gombocka = new Konva.Circle({
-    name: 'gombocka-' + i,
-    x: cx + i * scoreW,
-    y: scoreY,
-    radius: str,
-    fill: color(colorMap[i % players]),
-    opacity: 0.1
-  });
-  scoreBar.add(gombocka);
-  var score = new Konva.Text({
-    name: 'score-' + i,
-    x: cx + i * scoreW,
-    y: scoreY - fontSize - 2 * str,
-    text: '',
-    fontFamily: 'Dosis',
-    fontStyle: 'bold',
-    fontSize: fontSize,
-    fill: 'black'
-  });
-  score.setOffset({x: score.getWidth() / 2, y: 0});
-  scoreBar.add(score);
-  var lastm = new Konva.Text({
-    name: 'lastm-' + i,
-    x: cx + i * scoreW,
-    y: scoreY + 2 * str,
-    text: '',
-    fontFamily: 'Dosis',
-    fontStyle: 'bold',
-    fontSize: fontSize,
-    fill: 'black'
-  });
-  lastm.setOffset({x: lastm.getWidth() / 2, y: 0});
-  scoreBar.add(lastm);
-  var line = new Konva.Line({
-    name: 'line-' + i,
-    points: [cx + i * scoreW + str, scoreY, cx + i * scoreW + scoreW - str, scoreY],
-    fill: 'black',
-    stroke: 'black',
-    strokeWidth: 1,
-    opacity: 0.1
-  });
-  scoreBar.add(line);
 }
 
 function color(arr) {
